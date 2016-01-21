141db7d6a92e950eaa5563e71dafa511e7194646
Oliver Ponder <oliver.ponder@gmail.com>
2016-01-21 18:10:11 +0100

    Structure and auto reloading browser stuff

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+var log4js = require('log4js');
+var logger = log4js.getLogger('[Main]');
+
+log4js.configure({
+  appenders: [
+    {
+      type: 'console',
+      layout: {
+        type: 'basic'
+      }
+    }
+  ]
+});
+
+logger.info("Hi from main.js");
+
+var Game = require('./lib/game.js');
+
+window.Game = Game;
