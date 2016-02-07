e25285078262de0fec12c2f68ea2c02b4733c1a5
Oliver Ponder <oliver.ponder@gmail.com>
2016-02-07 12:54:22 +0100

    small change

--- src/js/lib/game.js
+++ src/js/lib/game.js
@@ -37,13 +37,14 @@ function Game() {
     gameCanvas.appendChild( renderer.domElement );
 
     this.player = new Player(input);
+    this.fireplace = new Fireplace();
     world.addEntity(this.player);
 
     world.addEntity(new House(-2, 1));
 
     world.addEntity(new House(12, 1));
 
-    this.fireplace = new Fireplace();
+
     world.addEntity(this.fireplace);
 
     world.loadChunks();
